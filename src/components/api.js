8301c0a3c432848d46006588c71fdca48d5bab3f
Rahul Mishra <rahulmishra@Rahuls-MacBook-Air.local>
2025-10-17 17:12:15 +0530

    Second commit:Birthday_Song_AI frontend with added file and updated code

--- src/components/api.js
+++ src/components/api.js
@@ -1,5 +1,5 @@
 
 
-export const url="http://localhost:5555"
+// export const url="http://localhost:5555"
 
-// export const url="https://birthday-song-ai-server.onrender.com"
+export const url="https://birthday-song-ai-server.onrender.com"
